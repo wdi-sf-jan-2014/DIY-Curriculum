67717efa54d276c1b08d7eb08c114661a1419cd3
ste claudino daffara <ste.daff@gmail.com>
2014-02-22 15:33:11 -0800

    generated a new rails app and added gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Diyc::Application.config.secret_key_base = '442a7eafae3987b8221200788f086f76f383d72578f98cf84685215abe6a992bfe8f1c4551e433ec173abb317472afc3362cfcd4a194d0c0764d3d108cfd5454'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Diyc::Application.config.session_store :cookie_store, key: '_diyc_session'
