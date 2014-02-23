31bde0dac7e895b6dc3533851e0529d79b5ca466
jeromeall <jeromeallouche01@gmail.com>
2014-02-22 19:32:42 -0800

    devise install generation & added default_url_options in environment files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,8 @@ Diyc::Application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -26,4 +28,6 @@ Diyc::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.action_mailer.default_url_options = { :host => '0.0.0.0:8080' }
 end
