046336d91abd79cc26329bece9a568f1a6623b03
ste claudino daffara <ste.daff@gmail.com>
2014-02-23 02:37:32 -0800

    added nested resources and section controller methods

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,3 +1,34 @@
 class SectionsController < ApplicationController
 before_filter :authenticate_user!, except: [:index, :show]
+
+  def index
+    course = Course.find(params[:course_id])
+    @sections = Course.all
+    @created_courses = createdCourses
+  end
+
+  def show
+    @section = Section.find(params[:id])
+  end
+
+  def create
+    new_section = params.require(:section).permit(:title)
+    section = Section.create(new_section)
+    course = Course.find(params[:course_id])
+    section.course_id = course.id
+    section.save
+
+    redirect_to course_sections_path
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+
 end
