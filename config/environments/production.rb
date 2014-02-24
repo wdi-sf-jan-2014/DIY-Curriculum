683c139464fb9161cc295b2c9a64ae5a4fee3f83
jeromeall <jeromeallouche01@gmail.com>
2014-02-24 11:48:45 -0800

    foundation_and_overrides file removed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,9 +2,6 @@ Diyc::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
 
-
-  config.assets.compile = true
-
   # Code is not reloaded between requests.
   config.cache_classes = true
 
