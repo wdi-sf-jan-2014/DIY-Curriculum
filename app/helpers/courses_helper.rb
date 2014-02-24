344988816aef8e6c9452f3c66e59fdc408d0e209
jeromeall <jeromeallouche01@gmail.com>
2014-02-23 18:59:23 -0800

    fixed created_courses method

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -2,12 +2,12 @@ module CoursesHelper
 
 def createdCourses
   all_courses = Course.all
-  # created_courses = all_courses.where(:user_id => user.id)
+  created_courses = all_courses.where(:user_id => current_user.id)
 
-  Course.all.each do |course|
-    if course.user_id = current_user.id
-    end
-  end
+  # Course.all.each do |course|
+  #   if course.user_id = current_user.id
+  #   end
+  # end
 end
 
 def enrolledCourses
