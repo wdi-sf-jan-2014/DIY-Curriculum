eee7ae720613e9801bf710547d528b563346e265
jeromeall <jeromeallouche01@gmail.com>
2014-02-24 11:12:14 -0800

    Use a fake devise secret key during precompile
    Revert configuration change that would disable initialization during precompile.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,10 +25,7 @@ Diyc::Application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
-
-  # Do not initialize on precompile
-  config.assets.initialize_on_precompile = false
-
+  
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
