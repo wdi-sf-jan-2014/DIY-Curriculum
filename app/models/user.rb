fe9450d672fe438990886582bfbbbe688982ad98
jeromeall <jeromeallouche01@gmail.com>
2014-02-23 17:02:42 -0800

    added dependent destroy for courses and comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :courses
-  has_many :comments
+  has_many :courses, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
 
