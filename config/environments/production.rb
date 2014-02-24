52a658f6571d6f54b13ab092de29a9e98ba9edca
jeromeall <jeromeallouche01@gmail.com>
2014-02-24 11:31:06 -0800

    moved config.assets.initialize_on_precompile = false to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 Diyc::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+
+  # Precompile assets disabled
+  config.assets.initialize_on_precompile = false
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
