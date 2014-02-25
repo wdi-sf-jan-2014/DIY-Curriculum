2aa4172e2f848a9c515027f69cc55a4285315e88
ste claudino daffara <ste.daff@gmail.com>
2014-02-25 13:44:06 -0800

    created enrollment model relationships

--- app/models/course.rb
+++ app/models/course.rb
@@ -13,9 +13,9 @@
 
 class Course < ActiveRecord::Base
 
-  belongs_to :user
   belongs_to :category
   has_many :sections, dependent: :destroy
   has_many :comments, as: :commentable, dependent: :destroy
+  has_many :users, through: :enrollments
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :courses, dependent: :destroy
+  has_many :courses, through: :enrollments, dependent: :destroy
   has_many :comments, dependent: :destroy
 end
 
