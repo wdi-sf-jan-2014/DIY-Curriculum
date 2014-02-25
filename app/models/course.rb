52d49bf8f56bbd3ae1c4afb28390643d32f93bd9
jeromeall <jeromeallouche01@gmail.com>
2014-02-24 17:21:25 -0800

    uncommented belongs_to :user

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,6 @@
 class Course < ActiveRecord::Base
 
-  # belongs_to :user
+  belongs_to :user
   belongs_to :category
   has_many :sections, dependent: :destroy
   has_many :comments, as: :commentable, dependent: :destroy
