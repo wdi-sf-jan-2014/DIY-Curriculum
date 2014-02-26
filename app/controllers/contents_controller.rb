c68bb05533af5ec1d4cebb4115f28f52d371de97
yutaka_houlette <yutakahoulette@gmail.com>
2014-02-25 20:07:00 -0800

    video works but not through iframe, yet...

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -6,6 +6,13 @@ def create
   section = Section.find(params[:section_id])
   @content = section.contents.create(new_content)
 
+  # if @content.source_url.include? "youtube.com" 
+  #   url = @content.source_url
+  #   last = url.length
+  #   first = last - 11
+  #   embed_code = url[("#{first}".to_i)..("#{last}".to_i)].to_s
+  #   @video_url = "http://www.youtube.com/embed/" + embed_code
+  # end
   respond_to do |f|
     f.json { render :json => @content}
   end
