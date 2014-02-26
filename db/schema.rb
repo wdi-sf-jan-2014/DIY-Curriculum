b943c39474cfdd6f11fce30a9d90ff9f93491783
yutaka_houlette <yutakahoulette@gmail.com>
2014-02-25 17:35:12 -0800

    old state

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140224004817) do
+ActiveRecord::Schema.define(version: 20140225223031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20140224004817) do
     t.integer  "course_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "position"
   end
 
   create_table "users", force: true do |t|
