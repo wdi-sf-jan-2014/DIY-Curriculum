c90cb8502e39f88f0ac13ad75ae191b42f5befd6
jeromeall <jeromeallouche01@gmail.com>
2014-02-26 11:36:20 -0800

    sidekiq worker performing async properly

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -6,12 +6,7 @@ def create
   new_content = params.require(:content).permit(:source_url, :text, :res_title, :res_author, :res_domain, :res_word_count, :res_excerpt, :res_content)
   section = Section.find(params[:section_id])
   @content = section.contents.create(new_content)
-  #ReadabilityWorker.perform_async(@content.id)    
-  url = @content.source_url
-  response = Typhoeus.get("https://www.readability.com/api/content/v1/parser?url=#{url}/&token=2e371692e3f7e9fd04045befb46b38364db35326")      
-  result = JSON.parse(response.body)
-  @content.update_attributes(res_title: result["title"], res_author: result["author"], res_domain: result["domain"], res_word_count: result["word_count"], res_excerpt: result["excerpt"], res_content: result["content"])
-  binding.pry
+  ReadabilityWorker.perform_async(@content.id)    
   respond_to do |f|
     f.json { render :json => @content}
   end
--- app/workers/readability_worker.rb
+++ app/workers/readability_worker.rb
@@ -7,13 +7,6 @@ class ReadabilityWorker
     response = Typhoeus.get("https://www.readability.com/api/content/v1/parser?url=#{url}/&token=2e371692e3f7e9fd04045befb46b38364db35326")      
     result = JSON.parse(response.body)
     @content.update_attributes(res_title: result["title"], res_author: result["author"], res_domain: result["domain"], res_word_count: result["word_count"], res_excerpt: result["excerpt"], res_content: result["content"])
-    # @content.res_title = 
-    # @content.res_author = result["author"]
-    # @content.res_domain = result["domain"]
-    # @content.res_word_count = result["word_count"]
-    # @content.res_excerpt = result["excerpt"]
-    # @content.res_content = result["content"]
-    # @content.save
   end
 
 end
