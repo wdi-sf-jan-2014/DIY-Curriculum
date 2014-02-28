9e8a378f2a9b0a95c230ef2421b26110ace3d421
ste claudino daffara <ste.daff@gmail.com>
2014-02-28 00:18:59 -0800

    added logo

---
+++ app/assets/javascripts/courses.js
@@ -0,0 +1,19 @@
+// $(function(){
+
+
+//     var maxHeight = -1;
+
+//     $('.columns-padding').each(function() {
+//       maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
+//       maxHeight += 5;
+//     });
+
+//   $(window).resize(function(){
+//        $('.columns-padding').each(function() {
+//        $(this).height(maxHeight);
+
+//      });
+
+//   });
+
+// });
